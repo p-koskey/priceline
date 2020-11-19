4d0b5b38d4d761167759b075ca8e2235a17200f6
nivlapeter <nivlapeter.0549@gmail.com>
2020-11-19 12:08:23 +0300

    add cors

--- priceline/settings.py
+++ priceline/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'bootstrap4',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -54,6 +55,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200/',
+    
 ]
 
 ROOT_URLCONF = 'priceline.urls'
