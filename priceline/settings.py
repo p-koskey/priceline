a21dda4d3c9abcdd810fb409b4c958d69c9bdec8
p-koskey <patiencekoskey@gmail.com>
2020-11-23 14:55:35 +0300

    added ghpages to access api

--- priceline/settings.py
+++ priceline/settings.py
@@ -127,6 +127,7 @@ AUTH_USER_MODEL ='authentication.User'
 #frontend 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
+    'https://p-koskey.github.io/priceline-front/',
     
 ]
 LANGUAGE_CODE = 'en-us'
