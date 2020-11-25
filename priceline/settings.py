6ba882629ad1ae07d123c61505746c29e51b527c
p-koskey <patiencekoskey@gmail.com>
2020-11-25 11:17:23 +0300

    change date format

--- priceline/settings.py
+++ priceline/settings.py
@@ -175,5 +175,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    "DATE_INPUT_FORMATS": ["%d-%m-%Y"],
     
 }
